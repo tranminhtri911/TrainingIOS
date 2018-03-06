bbcf7259a9a1b869d7ddd0f3cfabfa6584386e52
Tri <tranminhtri911@gmail.com>
2018-03-06 10:35:31 +0700

    Ex 1

---
+++ Lession 3/Ex 1/Ex1/Ex1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ex1
+//
+//  Created by Tran Tri on 3/6/18.
+//  Copyright © 2018 Tran Tri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
